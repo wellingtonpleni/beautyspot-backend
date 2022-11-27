63438150a9e3e48caec5a25100fc6a40995cf056
wellingtonpleni <wellingtonpleni@gmail.com>
2022-11-27 10:08:26 -0300

    Ajustes

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const URI = process.env.MONGODB_URL;
--- src/services/aws.js
+++ src/services/aws.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const AWS = require('aws-sdk');
 
 module.exports = {
-    IAM_USER_KEY: 'AKIAZH5DASLHNGWWTOMI',
-    IAM_USER_SECRET: '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',
-    BUCKET_NAME: 'beauty-spot-dev',
-    AWS_REGION: 'us-east-1',
+    IAM_USER_KEY: process.env.IAM_USER_KEY,
+    IAM_USER_SECRET: process.env.IAM_USER_SECRET,
+    BUCKET_NAME: process.env.BUCKET_NAME,
+    AWS_REGION: process.env.AWS_REGION,
     uploadToS3: function (file, filename, acl = 'public-read') {
         return new Promise((resolve, reject) => {
             let IAM_USER_KEY = this.IAM_USER_KEY;
